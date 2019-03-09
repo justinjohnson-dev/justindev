3d9dbb039b3bb67fec225d1ffb2b79a32de646fb
Justin Johnson <jjustin634@gmail.com>
2019-03-09 16:40:15 -0600

    adding javascript to add and remove nav bar background upon scroll

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if($(this).scrollTop() > 500) { 
-          $('.navbar').addClass('solid');
-      } else {
-          $('.navbar').removeClass('solid');
-      }
-    });
-});
