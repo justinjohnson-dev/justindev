d8b62b68781975ca93b350e79c24cdafb9734802
Justin Johnson <jjustin634@gmail.com>
2019-03-09 16:36:00 -0600

    added javascript to add and remove the border of the nav bar as you scroll down the page

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
