42366c7c4d7c75dab922e94215a625e807c27618
Justin Johnson <jjustin634@gmail.com>
2019-04-30 23:00:18 -0500

    hanging fadeIn and fadeOut times

--- javascript/rotatingWords.js
+++ javascript/rotatingWords.js
@@ -3,11 +3,11 @@ var colors = ["red", "teal", "orange", "navy"];
 var count = 1;
 
 setInterval(function(){    
-    $("span.keyword").fadeOut(1200, function(){        
+    $("span.keyword").fadeOut(600, function(){        
         $(this).html(keywords[count]).css("color", colors[count]);
         count++;        
         if(count == keywords.length)            
             count = 0;        
-        $(this).fadeIn(1200);    
+        $(this).fadeIn(600);    
     });
-}, 1000);
+}, 2000);
