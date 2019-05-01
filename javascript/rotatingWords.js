5285087800be90e8675c8b12aa971df50c17d530
Justin Johnson <jjustin634@gmail.com>
2019-04-30 22:03:14 -0500

    javascript file to create rotating word animations

---
+++ javascript/rotatingWords.js
@@ -0,0 +1,13 @@
+var keywords = ["~ Software Engineering Student ~", "~ Pursuing Bachelors in Computer Science  ~", "~ Computer Fanatic ~", "~ Ambitious Developer ~", "~ The Master Commander ~"];
+var colours = ["red", "teal", "green", "orange", "navy"];
+var count = 1;
+
+setInterval(function(){    
+    $("span.keyword").fadeOut(500, function(){        
+        $(this).html(keywords[count]).css("color", colours[count]);
+        count++;        
+        if(count == keywords.length)            
+            count = 0;        
+        $(this).fadeIn(500);    
+    });
+}, 2000);
