a751719526a4f5f33b553fd4b8266bd08c8dae83
Justin Johnson <jjustin634@gmail.com>
2019-04-30 22:02:40 -0500

    Adding java script to create rotating word animation

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-      if($(this).scrollTop() > 500) { 
+      if($(this).scrollTop() > 50) { 
           $('.navbar').addClass('solid');
       } else {
           $('.navbar').removeClass('solid');
       }
     });
 });
+
+/* This is a TODO */
