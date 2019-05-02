4d7b77c49413a068e864b8cadb8e43b2976905f2
Justin Johnson <jjustin634@gmail.com>
2019-05-01 19:59:41 -0500

    experimenting with the fadeIn and fadeOut speeds

--- javascript/rotatingWords.js
+++ javascript/rotatingWords.js
@@ -1,13 +1,13 @@
 var keywords = ["~ Software Engineering Student ~", "~ Pursuing Bachelors in Computer Science ~", "~ Computer Fanatic ~", "~ Ambitious Developer ~"];
-var colors = ["red", "teal", "orange", "navy"];
+var colors = ["white", "orange"];
 var count = 1;
 
 setInterval(function(){    
-    $("span.keyword").fadeOut(600, function(){        
+    $("span.keyword").fadeOut(500, function(){        
         $(this).html(keywords[count]).css("color", colors[count]);
         count++;        
         if(count == keywords.length)            
             count = 0;        
-        $(this).fadeIn(600);    
+        $(this).fadeIn(500);    
     });
-}, 2000);
+}, 2200);
